143930f3ad8658f3d5c0289d9ce2fd0dc2582168
mariya.tronina <mariya.tronina@rtlabs.ru>
2020-09-14 13:33:57 +0400

    домашнее задание 1 и 2: исправление замечаний

--- learn-otus-2/src/main/java/ru/otus/spring/config/AppConfig.java
+++ learn-otus-2/src/main/java/ru/otus/spring/config/AppConfig.java
@@ -1,6 +1,5 @@
 package ru.otus.spring.config;
 
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +19,6 @@ public class AppConfig {
     @Bean
     public Resource resource(@Value("${csv.file}") String csvName) {
         return new ClassPathResource(csvName);
-    }
-
-    @Bean
-    public CsvMapper csvMapper() {
-        return new CsvMapper();
     }
 
     @Bean
